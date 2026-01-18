d50b90d59dce389579e303d8228887100634ea7d
ozmoetr <ozmoetr@gmail.com>
2026-01-18 13:39:53 -0700

    Remove Web app and desktop folders

--- Web/client/src/types/colorthief.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'colorthief' {
-  export default class ColorThief {
-    getColor(img: HTMLImageElement, quality?: number): [number, number, number] | null;
-    getPalette(img: HTMLImageElement, colorCount?: number, quality?: number): [number, number, number][] | null;
-  }
-}
